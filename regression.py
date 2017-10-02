098c6662adcfe1696378e587ea7ff9956d644768
Youssef Swailem <youssefswailem@gmail.com>
2017-10-02 20:18:31 +0200

    Update regression problem

--- regression.py
+++ regression.py
@@ -1,22 +1,63 @@
 import numpy as np
 import pandas as pd
+from math import tanh
 
 flight_data = pd.read_csv('UA.csv')
-
-flight_data.dropna(axis=1, how='all', inplace=True)
 flight_data.dropna(axis=0, how='any', subset=[
-    'AIR_SYSTEM_DELAY',	'SECURITY_DELAY',	'AIRLINE_DELAY',	'LATE_AIRCRAFT_DELAY'	, 'WEATHER_DELAY'], inplace=True)
+                   'DISTANCE', 'ELAPSED_TIME'], inplace=True)
 
-feature = flight_data['DISTANCE']
-target = flight_data['ELAPSED_TIME']
+feature = np.array(flight_data['DISTANCE'])
+target = np.array(flight_data['ELAPSED_TIME'])
+learning_rate = 0.2
 
 weight = np.random.randn()
 bias = np.random.randn()
-print(weight == bias)
 
-training_num = 80 * len(feature) / 100
+# integer division to automatically floor
+training_num = 80 * len(feature) // 100
+
 train_x = feature[:training_num]
 train_y = target[:training_num]
 
 test_x = feature[training_num + 1:]
 test_y = target[training_num + 1:]
+
+mean_errors = []
+
+# Training
+for _ in range(10):
+
+    for i in range(len(train_x)):
+
+        inputs = train_x[i]
+        actual = train_y[i]
+
+        summation = np.dot(inputs, weight) - bias
+
+        # predicted = 1 / (1 + np.exp(-summation))  # sigmoid function
+        predicted = tanh(summation)
+        error = predicted - actual
+
+        weight -= learning_rate * error * inputs
+        bias -= learning_rate * error * -1
+
+    # mean_errors.append(error**2)
+    # mean = np.mean(mean_errors)
+    # print(mean)
+
+
+# Testing
+
+mean_errors = []
+
+for i in range(len(test_x)):
+
+    inputs = test_x[i]
+    actual = test_y[i]
+
+    summation = np.dot(inputs, weight) - bias
+    predicted = tanh(summation)
+    error = predicted - actual
+    mean_errors.append(error ** 2)
+
+print('Mean square of errors: ', np.mean(mean_errors))
