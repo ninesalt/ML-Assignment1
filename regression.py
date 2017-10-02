a65d5d2bcf6170a7063d9f6d47fe12709bc7b8be
Youssef Swailem <youssefswailem@gmail.com>
2017-10-02 17:16:22 +0200

    Add regression problem

--- regression.py
+++ regression.py
@@ -1,62 +1,22 @@
-from numpy import random, dot, array, arange
-from pandas import read_csv
-import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
-credit_cards = read_csv('creditcards.csv')
+flight_data = pd.read_csv('UA.csv')
 
-features = array(credit_cards[['V2', 'V11']])
-targets = array(credit_cards['Class'])
+flight_data.dropna(axis=1, how='all', inplace=True)
+flight_data.dropna(axis=0, how='any', subset=[
+    'AIR_SYSTEM_DELAY',	'SECURITY_DELAY',	'AIRLINE_DELAY',	'LATE_AIRCRAFT_DELAY'	, 'WEATHER_DELAY'], inplace=True)
 
-weights = random.randn(2)
-bias = random.randn()
+feature = flight_data['DISTANCE']
+target = flight_data['ELAPSED_TIME']
 
-learning_rate = 0.2
-error_tolerance = 0.14     # 0.14% error tolerance
-iterations = 0
-mispredictions = 100  # 100% wrong predictions
+weight = np.random.randn()
+bias = np.random.randn()
+print(weight == bias)
 
-while mispredictions > error_tolerance:
+training_num = 80 * len(feature) / 100
+train_x = feature[:training_num]
+train_y = target[:training_num]
 
-    iterations += 1
-    errors = 0
-
-    for i in range(len(features)):
-
-        inputs = features[i]
-        expected = targets[i]
-
-        summation = dot(inputs, weights) - bias
-        result = 1 if summation > 0 else 0
-
-        error = result - expected
-
-        weights -= learning_rate * error * inputs
-        bias -= learning_rate * error * -1
-
-        if error != 0:
-            errors += 1
-
-    # percent of wrong guesses
-    mispredictions = errors * 100 / len(features)
-
-    print('percent of mispredictions in iteration {} : {}% '.format(
-        iterations, mispredictions))
-
-print('Converged with learning rate of {} after {} iterations with an error tolerance of {}%'.format(
-    learning_rate, iterations, error_tolerance))
-
-
-# plotting
-v2 = [value for value, _ in features]
-v11 = [value for _, value in features]
-
-w0 = bias
-w1, w2 = weights
-x = arange(0, 20)
-y = (w0 / w2) - (w1 * x / w2)
-
-colors = [value / 255 for value in targets]
-
-plt.scatter(v2, v11, c=colors)
-plt.plot(x, y, 'k-')
-plt.show()
+test_x = feature[training_num + 1:]
+test_y = target[training_num + 1:]
